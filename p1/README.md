f72073cd1998b85ca1101500ebd617135b9b614a
Ali Lilienfeld <lilienfeld_alison@wheatoncollege.edu>
2023-09-19 21:47:08 +0000

    Created project and setup readme

---
+++ p1/README.md
@@ -0,0 +1,16 @@
+
+
+# Project 1
++ By: Ali Lilienfeld
++ URL: <http://e2p1.hesclassali.com>
+
+## Game planning
+
+
+
+
+
+## Outside resources
+
+
+## Notes for instructor
---
+++ p1/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Project 1</title>
+</head>
+
+<body>
+    <h2>Game Mechanics</h2>
+    <ul>
+        <li>Step 1</li>
+        <li>Step 2</li>
+    </ul>
+    <h2>Results</h2>
+    <ul>
+        <li>Step 1</li>
+        <li>Step 2</li>
+    </ul>
+
+</body>
+
+</html>
