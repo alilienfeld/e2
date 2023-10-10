64ac658e3af2ac7b3ea4fc508dbec1125911821e
Ali Lilienfeld <lilienfeld_alison@wheatoncollege.edu>
2023-10-10 23:37:11 +0000

    p1 v3 player 2 can now win

--- p1/index-view.php
+++ p1/index-view.php
@@ -10,14 +10,15 @@
     <ul>
         <li>Player 1 picks a card then chooses if the next card will be high or low</li>
         <li>Ties count as a win for the player</li>
-        <li>Player 1 plays until they lose</li>
+        <li>If Player 1 guesses correctly the turn switches to Player 2</li>
         <li>Player 2 then plays the same game</li>
-        <li>The player with the longest winning streak is the winner</li>
+        <li>The first Player to guess incorrectly loses</li>
     </ul>
     <h2>Results</h2>
     <ul>
-        <li>Player 1 had streak of <?php echo $player_1_wins?> correct guesses</li>
-        <li>Player 2 had a streak of <?php echo $player_2_wins?> correct guesses</li>
+        <li>Player 1 had streak of <?php echo count($player_1_wins)?> correct guesses</li>
+        <li>Player 2 had a streak of <?php echo count($player_2_wins)?> correct guesses</li>
+        <li><?php echo $loss?> guessed wrong first</li>
         <li>The winner is: <?php echo $winner?></li>
     </ul>
 
--- p1/index.php
+++ p1/index.php
@@ -1,26 +1,26 @@
 <?php
-// + Create an array of cards in a deck 
+// Create an array of cards in a deck 
 // represent ace as 1 jack-king as 11-13
 $deck = [1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10, 11, 11, 11, 11, 12, 12, 12, 12, 13, 13, 13, 13];
 shuffle($deck);
-// + Create an empty array of picked cards
-$player_1_cards = [];
-$player_2_cards = [];
-// + Create a player choice for high or low
+// Create an empty array of picked cards
+$player_1_wins = [];
+$player_2_wins = [];
+// Create a start card and start with player 1
 $player_turn = 'player_1';
 $chosen_card = array_pop($deck);
 $loss = null;
 
-//Keep playing high low until the player loses
+//Keep playing high low until a player loses
 while ($loss == null) {
     
-    // Given player best(ish) guess without counting cards
+    // Given player best(ish) guess without counting cards based on the chosen card
     if($chosen_card > 6) {
         $player_guess = 'low';
     } else {
         $player_guess = 'high';
     }
-     
+    // Determine what the correct answer is compared to the chosen card
     $next_card = array_pop($deck);
     if($next_card > $chosen_card) {
         $correct_answer = 'high';
@@ -29,31 +29,31 @@ while ($loss == null) {
     } else {
         $correct_answer = 'low';
     }
-    if($player_guess == $correct_answer or $correct_answer == 'tie') {
+    // Detemine if the guess was correct and rotate turns
+    if($player_guess == $correct_answer or $correct_answer == 'tie') { 
         if($player_turn == 'player_1') {
-            $player_1_cards[] = $chosen_card;
+            //echo $player_turn;
+            $player_1_wins[] = $chosen_card;
+            $chosen_card = $next_card;
             $player_turn = 'player_2';
-            $chosen_card = $next_card;
-        } else {
-            $player_2_cards[] = $chosen_card;
+        } else {  
+            //echo $player_turn; 
+            $player_2_wins[] = $chosen_card;
+            $chosen_card = $next_card;       
             $player_turn = 'player_1';
-            $chosen_card = $next_card;
         }
     } else {
         if($player_turn == 'player_1') {
-            $loss = 'player_1';
+            $loss = 'Player 1';
         } else {
-            $loss = 'player_2';
+            $loss = 'Player 2';
         }
     }
 }
-$player_1_wins = count($player_1_cards);
-$player_2_wins = count($player_2_cards);
-if($player_1_wins > $player_2_wins) {
-    $winner = 'Player 1';
-} elseif($player_1_wins == $player_2_wins) {
-    $winner = 'It is a tie!';
-} else {
+
+if($loss == 'Player 1') {
     $winner = 'Player 2';
+} else {
+    $winner = 'Player 1';
 }
 require 'index-view.php';
