b775384f97b0daff357e70950a9f0e3df8dcc54d
Ali Lilienfeld <lilienfeld_alison@wheatoncollege.edu>
2023-10-11 00:28:00 +0000

    checking for spelling and grammer

--- p1/index-view.php
+++ p1/index-view.php
@@ -9,7 +9,7 @@
     <h2>Game Mechanics</h2>
     <ul>
         <li>Player 1 picks a card then chooses if the next card will be high or low</li>
-        <li>Ties count as a win for the player</li>
+        <li>Ties count as a win for the player because we are not playing casino rules</li>
         <li>If Player 1 guesses correctly the turn switches to Player 2</li>
         <li>Player 2 the chooses based on the last card if it will be high or low</li>
         <li>The first Player to guess incorrectly loses</li>
@@ -20,13 +20,13 @@
         <li>On <?php echo $result['Turn'] ?> turn the card <?php echo $result['Drawn Card'] ?> was drawn.</li>
         <li><?php echo $result['Turn'] ?> guessed <b><?php echo $result['Player guess']?></b> for the next card.</li>
         <li>The next card drawn was <?php echo $result['Next Card']?> so the correct answer was
-            <b><?php echo $result['Correct Answer']?></b>
+            <b><?php echo $result['Correct Answer']?></b>.
         </li>
         <?php }?>
-        <li>Player 1 had streak of <?php echo count($player_1_wins)?> correct guesses</li>
-        <li>Player 2 had a streak of <?php echo count($player_2_wins)?> correct guesses</li>
-        <li><?php echo $loss?> guessed wrong first</li>
-        <li>The winner is: <?php echo $winner?></li>
+        <li>Player 1 had streak of <?php echo count($player_1_wins)?> correct guesses.</li>
+        <li>Player 2 had a streak of <?php echo count($player_2_wins)?> correct guesses.</li>
+        <li><?php echo $loss?> guessed wrong first.</li>
+        <li>The winner is: <?php echo $winner?>!</li>
     </ul>
 
 </body>
